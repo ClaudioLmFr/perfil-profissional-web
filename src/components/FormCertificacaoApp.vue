e25940e73febf8145624e053a1a27ebfeebabc2d
ClaudioLmFr <claudio182708@outlook.com>
2022-11-29 22:00:28 -0400

    tabelas-e-notificacoes

---
+++ src/components/FormCertificacaoApp.vue
@@ -0,0 +1,38 @@
+<template>
+    <button v-show="!formCertificacao" @click="formCertificacao = true" type="button">Novo</button>
+    <div v-show="formCertificacao">
+        <label for="instituição">Instituição de Ensino</label>
+        <input v-model="certificacao.instituicao" id="instituição" type="text" placeholder="Ex: High Tech Cursos">
+
+        <label for="titulo">Título</label>
+        <input v-model="certificacao.titulo" id="titulo" type="text" placeholder="Ex: Fábrica de programador">
+
+        <label for="cargaHoraria">Carga Horária</label>
+        <input v-model="certificacao.cargaHoraria" id="cargaHoraria" type="number" placeholder="Ex: 12 horas">
+
+        <button @click="adicionarcertificacao" type="button">Adicionar</button>
+        <button @click="formCertificacao = false" type="button">Cancelar</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        perfil: {
+            type: Object,
+            require: true,
+        },
+    },
+    data: () => ({
+        certificacao: {},
+        formCertificacao: false,
+    }),
+    methods: {
+        adicionarcertificacao() {
+            this.perfil.certificacoes.push(this.certificacao);
+            this.certificacao = {};
+            this.formCertificacao = false;
+        },
+    },
+}
+</script>
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import Home from './../pages/Home.vue'
 import Perfil from './../pages/Perfil.vue'
 import Login from './../pages/Login.vue'
 import NovaConta from './../pages/NovaConta.vue'
+import Notificacoes from '../pages/Notificacoes.vue'
 
 export default [
     { path: '/home', component: Home, name: "Home" },
     { path: '/perfil', component: Perfil, name: "Perfil" },
     { path: '/nova-conta', component: NovaConta, name: "NovaConta" },
     { path: '/', component: Login, name: "Login" },
+    { path: '/notificacoes', component: Notificacoes, name: "Notificacoes" }
 ]
